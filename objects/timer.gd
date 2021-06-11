e974287a2f5e7bfbb75d5c6f3bde52412f94f8b8
Mark <mark_kaldas@yahoo.com>
2021-06-11 17:31:47 -0600

    added global timer

---
+++ objects/timer.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+export var beats_per_second = 2.0
+
+var beat_interval = 1.0 / beats_per_second
+
+var timer = 0.0
+
+signal beat
+
+func _process(dt):
+	timer += dt
+	if timer >= beat_interval:
+		timer -= beat_interval
+		emit_signal("beat")
