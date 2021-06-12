4aff62a809b99251c444a5d7544ed0f89ffc4568
SADragonMaker <44631569+SADragonMaker@users.noreply.github.com>
2021-06-11 22:17:01 -0600

    moving and grid stuff

--- scenes/game.gd
+++ scenes/game.gd
@@ -1,14 +1,19 @@
 extends Node2D
 
-enum Option {NONE, MOVING_TO}
+enum Option {NONE, MOVING_TO, SPAWNING}
+
+export var fighter_prefab: PackedScene
+export var miner_prefab: PackedScene
+export var pylon_prefab: PackedScene
 
 var selecting_state = Option.NONE
 var selection: Node
 
-var currency: int = 0
+export var currency: int = 0
 
 func _unhandled_input(event):
 	if event is InputEventMouseButton && event.is_pressed():
+		$grid_selection_icon.position = Vector2(6, 6) + (get_global_mouse_position() / 12).floor() * 12
 		if selecting_state == Option.NONE:
 			var selected = false
 			for child in get_children():
@@ -19,12 +24,15 @@ func _unhandled_input(event):
 					var menu_offset = $commands.rect_pivot_offset
 					$commands.set_position(get_global_mouse_position() - menu_offset)
 					$commands.select(selection)
+					$spawning.hide()
 			if not selected:
 				selection = null
 				$commands.hide()
+				$spawning.set_position(get_global_mouse_position())
+				$spawning.visible = !$spawning.visible
 		else:
 			selecting_state = Option.NONE
-			# get mouse position and give move command
+			selection.receiver = "move " + String(6 + floor(get_global_mouse_position().x / 12) * 12) + " " + String(6 + floor(get_global_mouse_position().y / 12) * 12)
 
 func _on_move_pressed():
 	selecting_state = Option.MOVING_TO
@@ -47,6 +55,7 @@ func _on_defend_pressed():
 
 
 func _on_miner_pressed():
+	selection = miner_prefab.instance()
 	print("miner spawned")
 
 func _on_fighter_pressed():
