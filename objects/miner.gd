df7267a78b133ccd02c5efd8f70443909374e9b9
Mark <mark_kaldas@yahoo.com>
2021-06-12 13:08:58 -0600

    endless miner bugfixes

--- objects/miner.gd
+++ objects/miner.gd
@@ -1,6 +1,6 @@
 extends Pathfinder
 
-export var mine_radius = 12*6
+export var mine_radius = 12*12
 
 enum State { Idle, Moving, Mining, Disconnected }
 
@@ -13,9 +13,13 @@ var to_mine = [ ]
 var currently_mining: Ore
 var desired_ore: Ore
 
+signal mine 
+
 func _ready():
 	nav = get_node("../world/nav") as Navigation2D
 	target_pos = position
+
+	# connect("mine", get_owner().get_node("sfx_controller"), "_on_mine")
 
 func _process(dt):
 	if receiver != "":
@@ -23,7 +27,7 @@ func _process(dt):
 		receiver = ""
 
 func sort_by_distance(a, b):
-	return position.distance_squared_to(a.position) < position.distance_squared_to(b.position)
+	return is_instance_valid(a) and is_instance_valid(b) and (position.distance_squared_to(a.position) < position.distance_squared_to(b.position))
 
 func _on_receive(_receiver: String):
 	var splitted_receiver = _receiver.split(" ")
@@ -31,6 +35,11 @@ func _on_receive(_receiver: String):
 	if command == "move":
 		target_pos = Vector2(splitted_receiver[1].to_float(), splitted_receiver[2].to_float())
 		is_moving = true
+		to_mine = [ ]
+
+		if currently_mining:
+			currently_mining.in_use_by = null
+		currently_mining = null
 		mstate = State.Moving
 	elif command == "mine":
 
@@ -48,23 +57,44 @@ func _on_receive(_receiver: String):
 		elif mstate == State.Mining:
 			if currently_mining:
 				currently_mining.resources_left -= 1
-				get_owner().currency += 1
+				get_tree().root.get_child(0).currency += 1
+				emit_signal("mine")
 				if currently_mining.resources_left <= 0:
 					to_mine.erase(currently_mining)
+					currently_mining.in_use_by = null
 					currently_mining.queue_free()
 					currently_mining = null
-			elif desired_ore:
+			elif desired_ore and is_instance_valid(desired_ore):
 				target_pos = desired_ore.position
 				is_moving = true
 				move_towards_target()
 				if not is_moving:
 					currently_mining = desired_ore
+					currently_mining.in_use_by = self
 					desired_ore = null
 			elif to_mine.size() == 0:
 				mstate = State.Idle
 			else:
+				for ore in to_mine:
+					if not is_instance_valid(ore) or not ore.position:
+						to_mine.erase(ore)
+				
 				to_mine.sort_custom(self, "sort_by_distance")
-				desired_ore = to_mine[0]
+
+				if to_mine.size() == 0:
+					mstate = State.Idle
+					return
+
+				for ore in to_mine:
+					if not is_instance_valid(ore) or not ore.position:
+						to_mine.erase(ore)
+						continue
+
+					if ore.in_use_by == self or ore.in_use_by == null:
+						desired_ore = ore
+						ore.in_use_by = self
+						break
+				
 
 
 func _on_miner_mouse_entered():
