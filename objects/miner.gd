5e3c02a84045fddf0e597496b3723cf8544737b3
Benjamin Nielsen <56326234+ben-baqa@users.noreply.github.com>
2021-06-12 13:12:28 -0600

    Start of sfx

--- objects/miner.gd
+++ objects/miner.gd
@@ -18,8 +18,7 @@ signal mine
 func _ready():
 	nav = get_node("../world/nav") as Navigation2D
 	target_pos = position
-
-	# connect("mine", get_owner().get_node("sfx_controller"), "_on_mine")
+	connect("mine", get_owner().get_node("sfx_controller"), "_on_mine")
 
 func _process(dt):
 	if receiver != "":
