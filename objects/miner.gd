7aad3cdb39de0194b8434ab8e3b3a11d14d5e0c1
Mark <mark_kaldas@yahoo.com>
2021-06-11 21:50:17 -0600

    idk

--- objects/miner.gd
+++ objects/miner.gd
@@ -1,13 +1,24 @@
 extends Area2D
 
-enum State {Idle, Moving, Mining, Disconnected}
+enum State { Idle, Moving, Mining, Disconnected }
+
+var nav: Navigation2D
 
 var connected = true
 var receiver: String = ""
 var mstate: int = State.Idle
 var mouse_on: bool = false
 
-func _process(delta):
+var target_pos: Vector2
+var path: PoolVector2Array
+
+func resolve_path():
+	path = nav.get_simple_path(position, target_pos)
+
+func _ready():
+	nav = get_node("../world/nav") as Navigation2D
+
+func _process(dt):
 	if receiver != "":
 		_on_receive(receiver)
 		receiver = ""
@@ -16,7 +27,10 @@ func _on_receive(_receiver: String):
 	var splitted_receiver = _receiver.split(" ")
 	var command = splitted_receiver[0]
 	if command == "move":
-		var coord = Vector2(splitted_receiver[1].to_float(), splitted_receiver[2].to_float())
+		target_pos = Vector2(splitted_receiver[1].to_float(), splitted_receiver[2].to_float())
+		print("Command to move to: ")
+		print(target_pos)
+		resolve_path()
 		mstate = State.Moving
 	elif command == "mine":
 		mstate = State.Mining
@@ -28,3 +42,8 @@ func _on_miner_mouse_entered():
 
 func _on_miner_mouse_exited():
 	mouse_on = false
+
+func _on_global_timer_beat():
+	resolve_path()
+	if path and path[0]:
+		position = path[0]
