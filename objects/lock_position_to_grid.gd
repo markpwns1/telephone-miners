c2fcf139b0ba6b9f3f19366b11ea289f3c1663ec
SADragonMaker <44631569+SADragonMaker@users.noreply.github.com>
2021-06-12 16:18:47 -0600

    enemy fixed

--- objects/lock_position_to_grid.gd
+++ objects/lock_position_to_grid.gd
@@ -1,16 +1,8 @@
 extends Node2D
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
 var follow_mouse = true
 
 var follow_node: Node
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
 
 
 func _process(_delta):
