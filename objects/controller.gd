933dda2d990afdc79934eb940122ea895c6e5d8b
SADragonMaker <44631569+SADragonMaker@users.noreply.github.com>
2021-06-11 22:57:38 -0600

    spawning

--- objects/controller.gd
+++ objects/controller.gd
@@ -2,6 +2,9 @@ extends Area2D
 
 export var transmitting = []
 
+var mouse_on: bool = false
+var connected: bool = true # Constant
+
 func check():
 	for obj in transmitting:
 		get_node(obj).receiver = "check"
@@ -11,3 +14,11 @@ func _on_global_timer_beat():
 		if child.get("connected") != null:
 			child.connected = false
 	check()
+
+
+func _on_controller_mouse_entered():
+	mouse_on = true
+
+
+func _on_controller_mouse_exited():
+	mouse_on = false
--- scenes/game.gd
+++ scenes/game.gd
@@ -9,27 +9,36 @@ export var pylon_prefab: PackedScene
 var selecting_state = Option.NONE
 var selection: Node
 
+var spawning_position: Vector2
+
 export var currency: int = 0
 
 func _unhandled_input(event):
 	if event is InputEventMouseButton && event.is_pressed():
 		$grid_selection_icon.position = Vector2(6, 6) + (get_global_mouse_position() / 12).floor() * 12
-		if selecting_state == Option.NONE:
+		if selecting_state == Option.NONE or selecting_state == Option.SPAWNING:
 			var selected = false
 			for child in get_children():
 				if child.get("mouse_on") and child.get("connected"):
-					selected = true
-					selection = child
+					if selecting_state == Option.NONE:
+						selected = true
+						selection = child
 
-					var menu_offset = $commands.rect_pivot_offset
-					$commands.set_position(get_global_mouse_position() - menu_offset)
-					$commands.select(selection)
-					$spawning.hide()
+						var menu_offset = $commands.rect_pivot_offset
+						$commands.set_position(get_global_mouse_position() - menu_offset)
+						$commands.select(selection)
+						$spawning.hide()
+					elif child.get("transmitting") != null:
+						selected = true
+						child.transmitting.append(selection)
+						add_child(selection)
+						
 			if not selected:
 				selection = null
 				$commands.hide()
 				$spawning.set_position(get_global_mouse_position())
 				$spawning.visible = !$spawning.visible
+				spawning_position = $grid_selection_icon.position
 		else:
 			selecting_state = Option.NONE
 			selection.receiver = "move " + String(6 + floor(get_global_mouse_position().x / 12) * 12) + " " + String(6 + floor(get_global_mouse_position().y / 12) * 12)
@@ -53,13 +62,20 @@ func _on_defend_pressed():
 	selection = null
 	$commands.hide()
 
-
 func _on_miner_pressed():
 	selection = miner_prefab.instance()
-	print("miner spawned")
+	selection.position = spawning_position
+	selecting_state = Option.SPAWNING
+	$spawning.hide()
 
 func _on_fighter_pressed():
-	print("fighter spanwed")
+	selection = fighter_prefab.instance()
+	selection.position = spawning_position
+	selecting_state = Option.SPAWNING
+	$spawning.hide()
 
 func _on_pylon_pressed():
-	print("pylon spawned")
+	selection = pylon_prefab.instance()
+	selection.position = spawning_position
+	selecting_state = Option.SPAWNING
+	$spawning.hide()
